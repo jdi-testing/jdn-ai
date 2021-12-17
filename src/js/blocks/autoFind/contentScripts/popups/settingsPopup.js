74b98891089f64df5656bb7d06809d2ba5e85243
MariiaNebesnova <mariia_nebesnova@epam.com>
2021-12-17 22:13:35 +0300

    isolate popup styles

--- src/js/blocks/autoFind/contentScripts/popups/settingsPopup.js
+++ src/js/blocks/autoFind/contentScripts/popups/settingsPopup.js
@@ -30,6 +30,9 @@ export const settingsPopup = () => {
     });
     const settings = xpathConfig;
 
+    const wrapper = document.createElement("div");
+    wrapper.setAttribute("id", "jdn-popup-wrapper");
+
     const background = document.createElement("div");
     background.classList.add("jdn-popup-bg");
 
@@ -169,7 +172,8 @@ export const settingsPopup = () => {
     modal.append(main);
 
     background.append(modal);
-    document.body.append(background);
+    wrapper.append(background);
+    document.body.append(wrapper);
 
     chrome.storage.onChanged.addListener((event) => {
       if (event?.IS_DISCONNECTED?.newValue === true) {
