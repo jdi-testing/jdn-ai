22a3641158f5ba107504b3d45da4639f0e13bdb7
MariiaNebesnova <mariia_nebesnova@epam.com>
2021-12-21 10:49:56 +0300

    fix edit locator reducer

---
+++ src/js/blocks/autoFind/tests/locator.mock.js
@@ -0,0 +1,20 @@
+export const locator1 = {
+  attrId: "",
+  element_id: "8736312404689610766421832473",
+  height: 201.3333435059,
+  locator: {
+    fullXpath: "/html/body/div/div[1]/div/div[1]/div/div[1]/ul",
+    robulaXpath: "//*[@class='sidebar-menu left']",
+    taskStatus: "SUCCESS",
+  },
+  name: "radiobuttonsUl",
+  predictedAttrId: "",
+  predicted_label: "radiogroup",
+  predicted_probability: 1,
+  sort_key: 40266.6687011719,
+  tagName: "ul",
+  type: "RadioButtons",
+  width: 200,
+  x: 0,
+  y: 60,
+};
