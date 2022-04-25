674f0d725c8bad74cdb38ef27c886aa882538048
Anna Kuznetsova <anna_kuznetsova1@epam.com>
2022-04-25 09:07:26 +0300

    issue_396: fix locator copying

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -2,3 +2,8 @@ export const floatToPercent = (value) => {
   // wse need to show percents, but multiply float * 100 provides an unexpected result and leads to bugs
   return Math.trunc(value * 100);
 };
+
+export const copyToClipboard = (text) => {
+  const transformedText = text.replace(/'/g, "\\'").replace(/\n/g, '\\n');
+  chrome.devtools.inspectedWindow.eval(`copy('${transformedText}')`);
+}
