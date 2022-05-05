3a08698731feb795bb47af6952efea9224af3fb2
Anna Kuznetsova <anna_kuznetsova1@epam.com>
2022-05-05 17:40:07 +0300

    issue_425: refactoring

--- src/components/Locators/LocatorsTree.jsx
+++ src/components/Locators/LocatorsTree.jsx
@@ -1,50 +1,31 @@
 import React from "react";
-import { useSelector } from "react-redux";
+import { size } from "lodash";
 import { Tree } from "antd";
-import { DownOutlined } from '@ant-design/icons';
+import { DownOutlined } from "@ant-design/icons";
 
 import { Locator } from "./Locator";
 import { Notifications } from "./Notifications";
-import { selectCurrentPage } from "../../store/selectors/mainSelectors";
 import { generatedLocatorsTreeMock } from "../../__tests__/__mocks__/locatorsTree.mock";
 
 const { TreeNode } = Tree;
 
 export const LocatorsTree = () => {
-  const currentPage = useSelector(selectCurrentPage).page;
-  const xpathConfig = useSelector((state) => state.main.xpathConfig);
-
   const renderTreeNodes = (data) => {
     return data.map((element) => {
-        if (element.children) {
-            return (
-                <TreeNode
-                  title={      
-                    <Locator
-                      key={element.element_id}
-                      {...{ element, xpathConfig, currentPage }}
-                    />}
-                  key={element.key}
-                  dataRef={element}
-                >
-                    {renderTreeNodes(element.children)}
-                </TreeNode>
-            );
-        }
         return (
-                <TreeNode
-                  {...element}
-                  title={      
-                    <Locator
-                      key={element.element_id}
-                      {...{ element, xpathConfig, currentPage }}
-                    />}
-                  key={element.key}
-                  dataRef={element}
-                />
-              );
+          <TreeNode
+            key={element.key}
+            title={
+              <Locator
+                key={element.element_id}
+                {...{ element }}
+              />}
+          >
+            {size(element.children) ? renderTreeNodes(element.children) : null}
+          </TreeNode>
+        );
     });
-}
+  };
 
   return (
     <div className="jdn__locatorsList">
