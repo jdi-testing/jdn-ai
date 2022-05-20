ec921c8d9f105a64cbf47f9d7300fa1ce49e63ef
MariiaNebesnova <mariia_nebesnova@epam.com>
2022-05-20 14:03:25 +0300

    add mocked data to store

--- src/components/Locators/LocatorsTree.jsx
+++ src/components/Locators/LocatorsTree.jsx
@@ -8,28 +8,24 @@ import { Locator } from "./Locator";
 import { Notifications } from "./Notifications";
 import { convertListToTree } from "../../utils/helpers";
 import { selectCurrentPage } from "../../store/selectors/mainSelectors";
-import { locatorsListMock } from "../../__tests__/__mocks__/locatorsList.mock";
+import { selectPageObjLocatorsByProbability } from "../../store/selectors/pageObjectSelectors";
+// import { locatorsListMock } from "../../__tests__/__mocks__/locatorsList.mock";
 
 const { TreeNode } = Tree;
 
-export const LocatorsTree = () => {
+export const LocatorsTree = ({ pageObject: currentPageObject }) => {
   const currentPage = useSelector(selectCurrentPage).page;
   const xpathConfig = useSelector((state) => state.main.xpathConfig);
+  const locators = useSelector((_state) => selectPageObjLocatorsByProbability(_state, currentPageObject));
 
-  const locatorsTree = convertListToTree(locatorsListMock);
+  const locatorsTree = convertListToTree(locators);
 
   const renderTreeNodes = (data) => {
-    return data.map((element) =>
-      <TreeNode
-        key={element.element_id}
-        title={
-          <Locator
-          {...{ element, xpathConfig, currentPage }}
-          />}
-      >
+    return data.map((element) => (
+      <TreeNode key={element.element_id} title={<Locator {...{ element, xpathConfig, currentPage }} />}>
         {size(element.children) ? renderTreeNodes(element.children) : null}
       </TreeNode>
-    );
+    ));
   };
 
   return (
