9038d2a28d755796d4d34236baad743547416f81
MariiaNebesnova <mariia_nebesnova@epam.com>
2022-06-08 14:38:39 +0300

    add bootstrap option

--- src/components/PageObjects/GenerationButtons.jsx
+++ src/components/PageObjects/GenerationButtons.jsx
@@ -33,6 +33,7 @@ export const GenerationButtons = ({ pageObj }) => {
           >
             <Select.Option value={elementLibrary.MUI}>{libraryNames.MUI}</Select.Option>
             <Select.Option value={elementLibrary.HTML5}>{libraryNames.HTML5}</Select.Option>
+            <Select.Option value={elementLibrary.Bootstrap}>{libraryNames.Bootstrap}</Select.Option>
           </Select>
         </Space>
         <Space direction="horizontal" size={8}>
