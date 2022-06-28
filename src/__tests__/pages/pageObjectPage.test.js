05903e9438bdd0414266b250ac420fbd08ff7b30
MariiaNebesnova <mariia_nebesnova@epam.com>
2022-06-28 09:14:03 +0300

    fix tests

--- src/__tests__/pages/pageObjectPage.test.js
+++ src/__tests__/pages/pageObjectPage.test.js
@@ -18,6 +18,19 @@ describe("application launch", () => {
   );
 
   beforeAll(() => {
+    Object.defineProperty(window, 'matchMedia', {
+      writable: true,
+      value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+      })),
+    });
     jest.mock("@ant-design/icons");
     jest.spyOn(pageObject, "getPageAttributes").mockImplementation(() => [
       {
