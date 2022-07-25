ec3df1f32a081fa8ca88b35e877eb76e0d0dd74e
MariiaNebesnova <mariia_nebesnova@epam.com>
2022-07-25 09:14:00 +0300

    move css attaching to js

--- src/services/connector.js
+++ src/services/connector.js
@@ -117,7 +117,15 @@ class Connector {
         sendMessage.defineTabId(this.tabId);
         sendMessage.setClosedSession({ tabId: this.tabId, isClosed: false });
       }),
+      this.attachCSS("contentScripts.css"),
     ]);
+  }
+
+  attachCSS(file) {
+    return chrome.scripting.insertCSS({
+      target: { tabId: this.tabId },
+      files: [file],
+    }).catch((error) => error);
   }
 }
 
