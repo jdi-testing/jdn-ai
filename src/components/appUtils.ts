7e09ae462891cda6544d318c618d50cc6cce2408
MariiaNebesnova <mariia_nebesnova@epam.com>
2022-08-16 15:06:02 +0300

    optimize core components

---
+++ src/components/appUtils.ts
@@ -0,0 +1,17 @@
+import { connector, sendMessage } from "../services/connector";
+import { SCRIPT_ERROR } from "../utils/constants";
+
+export const checkSession = async (setStateCallback: (val: boolean) => void) => {
+    setStateCallback(false);
+    sendMessage.checkSession(null).then((payloads) => {
+      payloads.forEach((payload) => {
+        if (
+          payload &&
+          payload.message !== SCRIPT_ERROR.NO_CONNECTION &&
+          payload.tabId !== connector.tabId
+        ) {
+            setStateCallback(true);
+        }
+      });
+    });
+  };
