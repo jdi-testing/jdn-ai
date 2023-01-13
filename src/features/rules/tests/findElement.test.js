f33e44dc0341fae22b34145b94374efb790d4817
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-01-13 17:36:12 +0300

    codestyle

--- src/features/rules/tests/findElement.test.js
+++ src/features/rules/tests/findElement.test.js
@@ -8,45 +8,45 @@ import { iconButton } from "./mocks/iconButton.mock";
 import { manySimpleElements } from "./mocks/manySimpleElements.mock.js";
 
 const runQuery = (domSource, callback) => {
-    const dom = new JSDOM(domSource);
-    global.document = dom.window.document;
+  const dom = new JSDOM(domSource);
+  global.document = dom.window.document;
 
-    const selectors = getLibrarySelectors(VueRules);
-    findBySelectors();
-    chrome.runtime.sendMessage({ message: "FIND_BY_SELECTORS", param: selectors }, callback);
-}
+  const selectors = getLibrarySelectors(VueRules);
+  findBySelectors();
+  chrome.runtime.sendMessage({ message: "FIND_BY_SELECTORS", param: selectors }, callback);
+};
 
 test("finds simple Vuetify elements by rules", () => {
-    const checkResult = (data) => {
-        expect(data.length).toBe(3);
-        expect(data[0].predicted_label).toBe("button");
-        expect(data[1].predicted_label).toBe("icon");
-        expect(data[2].predicted_label).toBe("button");
-    }
-    runQuery(manySimpleElements, checkResult);
+  const checkResult = (data) => {
+    expect(data.length).toBe(3);
+    expect(data[0].predicted_label).toBe("button");
+    expect(data[1].predicted_label).toBe("icon");
+    expect(data[2].predicted_label).toBe("button");
+  };
+  runQuery(manySimpleElements, checkResult);
 });
 
 test("don't recognize content if detectContent: undefined", () => {
-    const checkResult = (data) => {
-        expect(data.length).toBe(1);
-        expect(data[0].predicted_label).toBe("button");
-    }
-    runQuery(iconButton, checkResult);
+  const checkResult = (data) => {
+    expect(data.length).toBe(1);
+    expect(data[0].predicted_label).toBe("button");
+  };
+  runQuery(iconButton, checkResult);
 });
 
 test("solve priority conflicts", () => {
-    const checkResult = (data) => {
-        expect(data[0].predicted_label).toBe("card");
-    }
-    runQuery(card, checkResult);
-})
+  const checkResult = (data) => {
+    expect(data[0].predicted_label).toBe("card");
+  };
+  runQuery(card, checkResult);
+});
 
 test("recognize content if detectContent: true", () => {
-    const checkResult = (data) => {
-        expect(data.length).toBe(3);
-        expect(data[0].predicted_label).toBe("card");
-        expect(data[1].predicted_label).toBe("button");
-        expect(data[2].predicted_label).toBe("button");
-    }
-    runQuery(card, checkResult);
-})
+  const checkResult = (data) => {
+    expect(data.length).toBe(3);
+    expect(data[0].predicted_label).toBe("card");
+    expect(data[1].predicted_label).toBe("button");
+    expect(data[2].predicted_label).toBe("button");
+  };
+  runQuery(card, checkResult);
+});
