5e2731b260961cb091cd0adc199135dad45f945c
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-02-02 11:25:10 +0300

    fix import

--- src/app/components/Backdrop.jsx
+++ src/app/components/Backdrop.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { identificationStatus } from "../../common/constants/constants";
-import "./Backdrop.less";
+import "../styles/backdrop.less";
 export const Backdrop = () => {
   const showBackdrop = useSelector((state) => state.main.showBackdrop);
   const status = useSelector((state) => state.locators.present.status);
