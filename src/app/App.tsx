d4d7081d04115e035708179245b77e3d0925659d
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-02-07 12:40:45 +0300

    convert App to TS

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -14,7 +14,7 @@ import { HttpEndpoint, request } from "../services/backend";
 import { checkSession, getSessionId } from "./utils/appUtils";
 import { selectCurrentPage } from "./main.selectors";
 import { clearAll } from "./main.slice";
-import { store } from "./store/store";
+import { RootState, store } from "./store/store";
 import { useOnTabUpdate } from "./utils/useOnTabUpdate";
 
 import { defineServer } from "./reducers/defineServer.thunk";
@@ -24,11 +24,14 @@ import "./styles/index.less";
 import { BackendStatus } from "./types/mainSlice.types";
 import { LocatorsPage } from "../features/locators/LocatorsPage";
 import { PageObjectPage } from "../features/pageObjects/PageObjectPage";
+import { locatorGenerationController } from "../features/locators/utils/locatorGenerationController";
+import { removeEmptyPageObjects } from "../features/locators/reducers/removeEmptyPageObjects.thunk";
+import { removeOverlay } from "../pageServices/pageDataHandlers";
 
 const App = () => {
   const [isInvalidSession, setIsInvalidSession] = useState(false);
-  const [template, setTemplate] = useState();
-  const backendAvailable = useSelector((state) => state.main.backendAvailable);
+  const [template, setTemplate] = useState<Blob | undefined>(undefined);
+  const backendAvailable = useSelector((state: RootState) => state.main.backendAvailable);
   const currentPage = useSelector(selectCurrentPage);
   const dispatch = useDispatch();
 
