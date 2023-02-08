61a93de9bddbc689ad3ad77dad00b391c0e6ee2e
Liudmila Arslanbekova <ludmila.belenko@gmail.com>
2023-02-08 12:41:16 +0300

    fixed display name lint issue

--- src/common/components/breadcrumbs/Breadcrumbs.tsx
+++ src/common/components/breadcrumbs/Breadcrumbs.tsx
@@ -5,7 +5,6 @@ import { selectCurrentPage } from "../../../app/main.selectors";
 import { PageType } from "../../../app/types/mainSlice.types";
 import { selectCurrentPageObject } from "../../../features/pageObjects/pageObject.selectors";
 
-// eslint-disable-next-line react/display-name
 export const Breadcrumbs = React.forwardRef((props, ref: ForwardedRef<HTMLDivElement>): ReactElement | null => {
   const currentPage = useSelector(selectCurrentPage);
   const pageObject = useSelector(selectCurrentPageObject);
@@ -19,3 +18,5 @@ export const Breadcrumbs = React.forwardRef((props, ref: ForwardedRef<HTMLDivEle
     </div>
   ) : null;
 });
+
+Breadcrumbs.displayName = "Breadcrumbs";
