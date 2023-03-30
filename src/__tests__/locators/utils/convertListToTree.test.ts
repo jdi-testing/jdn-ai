3842d69cde76c46be950d5879b308578a41b336e
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-03-30 16:04:18 +0300

    codestyle

--- src/__tests__/locators/utils/convertListToTree.test.ts
+++ src/__tests__/locators/utils/convertListToTree.test.ts
@@ -4,15 +4,14 @@ import { locatorsListMock } from "../../__mocks__/locatorsList.mock";
 import { locatorsTreeMock } from "../../__mocks__/locatorsTree.mock";
 import { locatorsTreeMockSearch } from "../../__mocks__/locatorsTreeSearch.mock";
 
-
 describe("convertListToTree function", () => {
-    test("convert list to tree", () => {
-      const tree = convertListToTree(locatorsListMock);
-      expect(tree).toEqual(locatorsTreeMock);
-    });
-
-    test("convert list to tree with a searchString", () => {
-      const tree = convertListToTree(locatorsListMock, "Li7");
-      expect(tree).toEqual(locatorsTreeMockSearch);
-    });
+  test("convert list to tree", () => {
+    const tree = convertListToTree(locatorsListMock);
+    expect(tree).toEqual(locatorsTreeMock);
   });
+
+  test("convert list to tree with a searchString", () => {
+    const tree = convertListToTree(locatorsListMock, "Li7");
+    expect(tree).toEqual(locatorsTreeMockSearch);
+  });
+});
