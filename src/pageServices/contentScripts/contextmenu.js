cae4e8af8f463cb97cbd8f8e96d158d6d97342ac
Liudmila Arslanbekova <ludmila.belenko@gmail.com>
2023-06-01 14:19:57 +0400

    updated icons

--- src/pageServices/contentScripts/contextmenu.js
+++ src/pageServices/contentScripts/contextmenu.js
@@ -306,7 +306,6 @@ export const runContextMenu = () => {
         ? [
             {
               text: "Add to Page Object",
-              // icon: `<i class="ph-light ph-plus-circle green"></i>`,
               icon: `<svg width="12" height="12" viewBox="0 0 12 12" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M6 10.5C8.48528 10.5 10.5 8.48528 10.5 6C10.5 3.51472 8.48528 1.5 6 1.5C3.51472 1.5 1.5 3.51472 1.5 6C1.5 8.48528 3.51472 10.5 6 10.5Z" stroke="#5A5A5A" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M4.125 6H7.875" stroke="#5A5A5A" stroke-linecap="round" stroke-linejoin="round"/>
@@ -361,7 +360,7 @@ export const runContextMenu = () => {
             },
           ]
         : []),
-        ...(isGroup()
+      ...(isGroup()
         ? [
             {
               type: ContextMenu.DIVIDER,
