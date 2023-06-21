56b98f3ea98b3bed40b8741a7e0b673ba4bcf146
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-06-21 17:16:55 +0300

    check and restore hash on Retry

--- src/features/locators/reducers/rerunGeneration.thunk.ts
+++ src/features/locators/reducers/rerunGeneration.thunk.ts
@@ -1,14 +1,19 @@
 import { ActionReducerMapBuilder, createAsyncThunk } from "@reduxjs/toolkit";
 import { MaxGenerationTime } from "../../../app/types/mainSlice.types";
-import { Locator, LocatorsState } from "../../../features/locators/types/locator.types";
+import { Locator, LocatorTaskStatus, LocatorsGenerationStatus, LocatorsState } from "../../../features/locators/types/locator.types";
 import { runXpathGeneration } from "./runXpathGeneration.thunk";
+import { sendMessage } from "../../../pageServices/connector";
 
 interface Meta {
   generationData: Locator[];
   maxGenerationTime?: MaxGenerationTime;
 }
 
-export const rerunGeneration = createAsyncThunk("locators/rerunGeneration", (meta: Meta, thunkAPI) => {
+export const rerunGeneration = createAsyncThunk("locators/rerunGeneration", async (meta: Meta, thunkAPI) => {
+  if (meta.generationData.length === 1 && meta.generationData[0].locator.taskStatus === LocatorTaskStatus.FAILURE) {
+    const { jdnHash, locator: {xPath} } = meta.generationData[0];
+    await sendMessage.assignJdnHash({ jdnHash, locator: xPath });
+  }
   return thunkAPI.dispatch(runXpathGeneration(meta));
 });
 
