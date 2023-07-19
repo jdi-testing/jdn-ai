d9473cb301990f0b55efe8f95b18ff0802a9df3e
MariiaNebesnova <mariia_nebesnova@epam.com>
2023-07-19 17:03:37 +0300

    minor

--- src/features/locators/utils/runCssSelectorGeneration.ts
+++ src/features/locators/utils/runCssSelectorGeneration.ts
@@ -1,7 +1,7 @@
 import { sendMessage } from "../../../pageServices/connector";
-import { Locator } from "../types/locator.types";
+import { Locator, LocatorsGenerationStatus } from "../types/locator.types";
 
 export const runCssSelectorGeneration = async (locators: Locator[]) => {
   sendMessage.generateSelectorGroupByHash({ elements: locators, fireCallbackMessage: true });
-  return Promise.resolve("started");
+  return Promise.resolve(LocatorsGenerationStatus.started);
 };
