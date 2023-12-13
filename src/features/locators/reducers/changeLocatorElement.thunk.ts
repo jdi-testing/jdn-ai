81b44d7ddee899b0c1278e30f74d1548e7ebfb97
Anna Iustus <ajustusa@gmail.com>
2023-12-13 21:41:57 +0100

    feat: rewrite change locator element thunk

--- src/features/locators/reducers/changeLocatorElement.thunk.ts
+++ src/features/locators/reducers/changeLocatorElement.thunk.ts
@@ -1,9 +1,9 @@
 import { ActionReducerMapBuilder, createAsyncThunk } from '@reduxjs/toolkit';
-import { LocatorTaskStatus, LocatorsState } from '../types/locator.types';
+import { ILocator, LocatorTaskStatus, LocatorsState } from '../types/locator.types';
 import { locatorsAdapter, selectLocatorById } from '../selectors/locators.selectors';
 import { RootState } from '../../../app/store/store';
 import { ChangeLocatorAttributesPayload } from '../locators.slice';
-import { evaluateXpath, generateSelectorByHash, evaluateStandardLocator } from '../utils/utils';
+import { generateSelectorByHash, evaluateLocator } from '../utils/utils';
 import { generateId, getElementFullXpath } from '../../../common/utils/helpers';
 import { sendMessage } from '../../../pageServices/connector';
 import { LocatorType } from '../../../common/types/common';
@@ -16,71 +16,81 @@ export const changeLocatorElement = createAsyncThunk(
     // ToDo: fix legacy naming
     // eslint-disable-next-line @typescript-eslint/naming-convention
     const { locator, element_id, locatorType, ...rest } = payload;
-    const isCSSLocator = locatorType === LocatorType.cssSelector;
-    let foundHash;
-    let foundElementText;
-    let cssSelector;
-    let fullXpath;
-
-    const isStandardLocator: boolean =
-      locatorType === LocatorType.cssSelector ||
-      locatorType === LocatorType.className ||
-      locatorType === LocatorType.id ||
-      locatorType === LocatorType.linkText ||
-      locatorType === LocatorType.name ||
-      locatorType === LocatorType.tagName ||
-      locatorType.startsWith('data-');
-
-    if (isStandardLocator) {
-      ({ foundHash, foundElementText } = JSON.parse(await evaluateStandardLocator(locator, locatorType, element_id)));
-    } else {
-      ({ foundHash, foundElementText } = JSON.parse(await evaluateXpath(locator, element_id)));
-    }
 
     const state = thunkAPI.getState() as RootState;
-    // ToDo: fix legacy naming
-    // eslint-disable-next-line @typescript-eslint/naming-convention
-    const _locator = selectLocatorById(state, element_id);
+    const currentLocator = selectLocatorById(state, element_id);
 
-    if (!_locator) return;
+    if (!currentLocator) return;
+
+    const isDataAttributeLocator = locatorType === LocatorType.dataAttributes || locatorType.startsWith('data-');
+    const isCSSLocator = locatorType === LocatorType.cssSelector;
+    const isXPathLocator = locatorType === LocatorType.xPath;
+    const isStandardLocator =
+      [
+        LocatorType.cssSelector,
+        LocatorType.className,
+        LocatorType.id,
+        LocatorType.linkText,
+        LocatorType.name,
+        LocatorType.tagName,
+        LocatorType.dataAttributes,
+      ].includes(locatorType) || locatorType.startsWith('data-');
+
+    let foundHash, foundElementText, cssSelector, fullXpath;
+
+    ({ foundHash, foundElementText } = JSON.parse(await await evaluateLocator(locator, locatorType, element_id)));
 
     if (!foundHash) {
       foundHash = generateId();
       await sendMessage
         .assignJdnHash({ jdnHash: foundHash, locator, isCSSLocator })
         .then((res) => {
-          if (res === 'success') return res;
-          else throw new Error('Failed to assign jdnHash');
+          if (res !== 'success') throw new Error('Failed to assign jdnHash');
         })
-        .catch((err) => {
-          console.log(err);
-        });
+        .catch((err) => console.log(err));
     }
 
-    isCSSLocator
-      ? (fullXpath = await getElementFullXpath(foundHash))
-      : ({ cssSelector } = await generateSelectorByHash(element_id, foundHash));
-
-    const newValue = {
-      ..._locator,
-      ...rest,
+    const newValue: ILocator = {
+      ...currentLocator,
+      locator: { ...currentLocator.locator, output: locator },
       locatorType,
+      ...rest,
       elemText: foundElementText || '',
       jdnHash: foundHash,
-      locator: {
-        ...(isCSSLocator ? { cssSelector: locator, xPath: fullXpath } : { xPath: locator, cssSelector }),
-        ...(isCSSLocator
-          ? { cssSelectorStatus: LocatorTaskStatus.SUCCESS }
-          : { xPathStatus: LocatorTaskStatus.SUCCESS }),
-      },
     };
 
+    if (isCSSLocator) {
+      fullXpath = await getElementFullXpath(foundHash);
+      newValue.locator.cssSelector = locator;
+      newValue.locator.xPath = fullXpath;
+      newValue.locator.cssSelectorStatus = LocatorTaskStatus.SUCCESS;
+    } else if (isXPathLocator) {
+      cssSelector = (await generateSelectorByHash(element_id, foundHash)).cssSelector;
+      newValue.locator.cssSelector = cssSelector;
+      newValue.locator.xPath = locator;
+      newValue.locator.xPathStatus = LocatorTaskStatus.SUCCESS;
+    } else if (isStandardLocator) {
+      if (isDataAttributeLocator) {
+        newValue.locator.attributes = {
+          ...newValue.locator.attributes,
+          dataAttributes: { ...(newValue.locator.attributes?.dataAttributes || {}) },
+        }; // unfreeze object
+
+        if (newValue.locator.attributes.dataAttributes) {
+          newValue.locator.attributes.dataAttributes[locatorType] = locator;
+        }
+      } else {
+        const attributes = { ...newValue.locator.attributes }; // unfreeze object
+        attributes[locatorType] = locator;
+        newValue.locator.attributes = attributes;
+      }
+    }
     return newValue;
   },
 );
 
 export const changeLocatorElementReducer = (builder: ActionReducerMapBuilder<LocatorsState>) => {
-  return builder
+  builder
     .addCase(changeLocatorElement.fulfilled, (state, { payload }) => {
       // @ts-ignore
       locatorsAdapter.upsertOne(state, payload);
