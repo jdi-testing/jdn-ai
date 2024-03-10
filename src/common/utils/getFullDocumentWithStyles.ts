27a57925b8fac323055e01a2dd0c77288a12a97f
Anna Iustus <ajustusa@gmail.com>
2024-03-10 14:50:07 +0100

    feat: add new logic for fetching full document with styles into app

--- src/common/utils/getFullDocumentWithStyles.ts
+++ src/common/utils/getFullDocumentWithStyles.ts
@@ -1,5 +1,4 @@
 import connector from '../../pageServices/connector';
-import { fetchCSS } from '../../pageServices/contentScripts/fetchCSS';
 
 async function waitForAllStylesToLoad() {
   const styleSheets = document.querySelectorAll('link[rel="stylesheet"]');
@@ -24,45 +23,34 @@ export const getFullDocumentWithStyles = async () => {
   await waitForAllStylesToLoad();
 
   const documentResult = await connector.attachContentScript(() => {
-    const fetchAndInlineStyles = async () => {
-      const styleSheets = document.querySelectorAll('link[rel="stylesheet"]');
-      const stylePromises = Array.from(styleSheets).map(async (link) => {
-        const linkElement = link as HTMLLinkElement;
-        if (linkElement.href) {
-          return fetchCSS(linkElement.href)
-            .then((cssText) => `<style>${cssText}</style>`)
-            .catch((error) => {
-              console.error('Failed to load CSS:', error);
-              return '';
-            });
+    const fetchCSS = () => {
+      console.log('fetchCSS  is loaded');
+      let allStyles = '';
+      for (let i = 0; i < document.styleSheets.length; i++) {
+        const sheet = document.styleSheets[i];
+        try {
+          const rules = sheet.rules || sheet.cssRules;
+          for (let j = 0; j < rules.length; j++) {
+            allStyles += rules[j].cssText + '\n';
+          }
+        } catch (e) {
+          console.error("Can't  fetch styles: ", e);
         }
-        return Promise.resolve('');
-      });
-      return Promise.all(stylePromises);
+      }
+      return allStyles;
     };
 
-    let inlineStyles: string[] = [];
-    fetchAndInlineStyles()
-      .then((res) => {
-        inlineStyles = res;
-      })
-      .catch((e) => {
-        console.error('Something went wrong: ', e);
-        return []; // Возвращает пустой массив в случае ошибки
-      });
     let outerHTML = document.documentElement.outerHTML;
 
-    const stylesString = inlineStyles.join('\n');
+    const stylesString = fetchCSS();
     outerHTML = outerHTML.replace(/<link rel="stylesheet"[^>]+>/g, '');
-    outerHTML = outerHTML.replace('</head>', `${stylesString}</head>`);
+    outerHTML = outerHTML.replace('</head>', `<style>\n${stylesString}</style></head>`);
 
     const scripts = [...document.scripts]
       .map((script: HTMLScriptElement) => {
         return script.src ? `<script src="${script.src}"></script>` : `<script>${script.innerHTML}</script>`;
       })
       .join('\n');
-
-    console.log('stylesString: ', stylesString);
 
     outerHTML = outerHTML.replace('</body>', `${scripts}</body>`);
 
@@ -74,8 +62,3 @@ export const getFullDocumentWithStyles = async () => {
   console.log(outerHTML);
   return JSON.stringify(outerHTML);
 };
-
-// const result = await documentResult[0].result;
-// const { outerHTML } = JSON.parse(result);
-// console.log(outerHTML);
-// return JSON.stringify(outerHTML);
