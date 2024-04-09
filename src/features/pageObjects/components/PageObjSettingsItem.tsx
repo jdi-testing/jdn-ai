b578cd87862813eba55a19dfbe645078d3f1abdf
Alinochka07 <alina011988@icloud.com>
2024-04-09 15:11:46 +0600

    fix: align tooltips, seams
    Squashed update version into align tooltips, seams

--- src/features/pageObjects/components/PageObjSettingsItem.tsx
+++ src/features/pageObjects/components/PageObjSettingsItem.tsx
@@ -1,5 +1,8 @@
-import { Row, Col, Typography, Select } from 'antd';
+import { Row, Col, Typography, Select, Tooltip } from 'antd';
 import { InternalSelectProps } from 'antd/es/select';
+import { useSelector } from 'react-redux';
+import { FrameworkType } from '../../../common/types/common';
+import { selectCurrentPageObject } from '../selectors/pageObjects.selectors';
 import React from 'react';
 
 type Props = InternalSelectProps & {
@@ -7,21 +10,33 @@ type Props = InternalSelectProps & {
 };
 
 const PageObjSettingsItem = ({ label, disabled, id, value, defaultValue, onChange, options }: Props) => {
+  const currentPageObject = useSelector(selectCurrentPageObject);
+  const isCurrentFrameworkVividus = currentPageObject?.framework === FrameworkType.Vividus;
+  const selectContent = (
+    <Select
+      className="jdn__select"
+      disabled={disabled}
+      id={id}
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      options={options}
+    />
+  );
+
   return (
     <Row>
       <Col flex="104px">
         <Typography.Text style={disabled ? { color: 'rgba(0, 0, 0, 0.25)' } : {}}>{label}</Typography.Text>
       </Col>
       <Col flex="auto">
-        <Select
-          className="jdn__select"
-          disabled={disabled}
-          id={id}
-          value={value}
-          defaultValue={defaultValue}
-          onChange={onChange}
-          options={options}
-        />
+        {isCurrentFrameworkVividus && id === 'annotationType' ? (
+          <Tooltip placement="bottom" title="Not available for Vividus framework">
+            {selectContent}
+          </Tooltip>
+        ) : (
+          selectContent
+        )}
       </Col>
     </Row>
   );
