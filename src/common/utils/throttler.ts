98903b2b8a8841e53b57b3331f99851425d781b7
Anna Iustus <ajustusa@gmail.com>
2024-05-10 10:29:36 +0200

    feat: update throttler

--- src/common/utils/throttler.ts
+++ src/common/utils/throttler.ts
@@ -1,8 +1,9 @@
-import { Middleware } from '@reduxjs/toolkit';
+import { type Middleware } from '@reduxjs/toolkit';
 import { selectAreInProgress } from '../../features/locators/selectors/locatorsByPO.selectors';
+import { CssSelectorsGenerationPayload, XpathMultipleGenerationPayload } from '../../services/webSocketMessageHandler';
 
 class Throttler {
-  accumulatedArgs: any[] = [];
+  accumulatedArgs: (CssSelectorsGenerationPayload | XpathMultipleGenerationPayload)[] = [];
 
   interval: NodeJS.Timeout | null = null;
 
@@ -10,33 +11,36 @@ class Throttler {
     this.accumulateAndThrottle = this.accumulateAndThrottle.bind(this);
   }
 
-  accumulateAndThrottle(fn: (arg: any[]) => any) {
-    const debouncedFn = (args: any[]) => {
-      this.accumulatedArgs.push(...args);
+  accumulateAndThrottle(fn: (arg: CssSelectorsGenerationPayload | XpathMultipleGenerationPayload) => any) {
+    const debouncedFn = (args: CssSelectorsGenerationPayload | XpathMultipleGenerationPayload) => {
+      this.accumulatedArgs.push(args);
     };
 
     const throttledFn = () => {
       if (this.accumulatedArgs.length === 0) return;
 
       try {
-        fn(this.accumulatedArgs);
+        this.accumulatedArgs.forEach((arg) => fn(arg));
+        this.accumulatedArgs = [];
       } catch (error) {
         this.quitThrottler();
         if (__DEV_ENVIRONMENT__) console.warn("Can't invoke throttled function:", error);
       }
-      this.accumulatedArgs = [];
     };
 
     if (!this.interval) this.interval = setInterval(throttledFn, 500);
 
-    return (args: any) => {
+    return (args: CssSelectorsGenerationPayload | XpathMultipleGenerationPayload) => {
       debouncedFn(args);
     };
   }
 
   quitThrottler() {
-    if (this.interval) clearInterval(this.interval);
-    this.interval = null;
+    if (this.interval) {
+      clearInterval(this.interval);
+      this.interval = null;
+      this.accumulatedArgs = [];
+    }
   }
 }
 
