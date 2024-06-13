47d9523b73e7f616b6a04ef556fd4d7f05eddcd7
Anna Iustus <ajustusa@gmail.com>
2024-06-13 08:47:27 +0200

    fix: update compatible minor version of backend

--- src/app/utils/compatibleVersions.ts
+++ src/app/utils/compatibleVersions.ts
@@ -1,3 +1,3 @@
 export const compatibleMajorVer = 0;
-export const compatibleMinorVer = 2;
-export const compatibleBuildVer = 62;
+export const compatibleMinorVer = 3;
+export const compatibleBuildVer = 0;
